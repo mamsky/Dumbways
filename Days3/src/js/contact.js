d60fb643dd3c33be577b2f824f0b1fca3523e7cf
mamsky <pasteprosmana189@gmail.com>
2024-12-04 11:44:39 +0700

    Days 3

--- Days3/src/js/contact.js
+++ Days3/src/js/contact.js
@@ -2,7 +2,10 @@ var handleClick = document.getElementById("handleForm")
 
 handleClick.addEventListener('submit', (e) =>{
     e.preventDefault()
+    submitForm(e)
+})
 
+const submitForm = (e) =>{
     var form = e.target
     var formData = new FormData(form)
 
@@ -13,4 +16,4 @@ handleClick.addEventListener('submit', (e) =>{
     link.href=`mailto:${data.email}?subject=${data.subject}&body=Selamat Siang.%0D%0ANama: ${data.name}. %0D%0ANo Hp: ${data.number}.%0D%0ASkill: ${data.skill}.%0D%0A%0D%0ABerikut pesan saya:%0D%0A${data.subject}`
     
     link.click()
-})
+}
